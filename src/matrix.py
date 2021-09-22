9d0645b541c4929194467ab8f5ebaaf84cae7ed3
Celeste Acosta <89424776+ca3-10@users.noreply.github.com>
2021-09-22 05:47:40 +0000

    cofact det fixed (.pop()), test cases done

--- src/matrix.py
+++ src/matrix.py
@@ -1,7 +1,7 @@
 class Matrix:
 
-    def __init__(self, element): 
-        self.elements = element
+    def __init__(self, elements): 
+        self.elements = elements
         self.num_rows = len(self.elements)
         self.num_cols = len(self.elements[0])
     
@@ -55,27 +55,46 @@ class Matrix:
         copy_matrix = [[num for num in row] for row in self.elements]
         return Matrix(copy_matrix)
 
-    def minor_calc(self,row_index, col_index):
+    def minor_calc(self, col_index):
         minor_matrix = self.copy()
         for i in range(1,minor_matrix.num_cols):
-            minor_matrix.elements[i].remove(minor_matrix.elements[i][col_index])
-        minor_matrix.elements.remove(minor_matrix.elements[row_index])
-        return minor_matrix 
+            # print("element to be removed:", minor_matrix.elements[i][col_index])
+            minor_matrix.elements[i].pop(col_index)
+            #print("row =", i)
+            #print("column =", col_index)
+            #minor_matrix.print()
+            #print()
+        minor_matrix.elements.remove(minor_matrix.elements[0])
+        return minor_matrix
         
     def calc_determinant(self): 
+        #print('Calling calc_determinant on the following matrix:')
+        #self.print()
+        #print()
+
         copy_matrix = self.copy()
-        if copy_matrix.num_rows == 2 and copy_matrix.num_cols == 2: 
-            determinant = (self.elements[0][0] * copy_matrix.elements[1][1]) - (copy_matrix.elements[0][1] * self.elements[1][0])
+
+        if copy_matrix.num_rows != copy_matrix.num_cols: 
+            return "not a valid input, matrix must be square"
+            
+        elif copy_matrix.num_rows == 2 and copy_matrix.num_cols == 2: 
+            determinant = (copy_matrix.elements[0][0] * copy_matrix.elements[1][1]) - (copy_matrix.elements[0][1] * copy_matrix.elements[1][0])
             return determinant
+
         else: 
             det = 0 
-            for i in range(self.num_cols):
+            for i in range(copy_matrix.num_cols):
                 coefficent_sign = (-1) ** i
-                coefficent = copy_matrix.elements[0][i]
-                minor = self.minor_calc(0,i)
+                coefficent = self.elements[0][i]
+                minor = self.minor_calc(i)
+
+                #print('i = ', i)
+                #print('Coefficient =', coefficent)
+                #print('Smaller matrix associated with coefficient above:')
+                #minor.print()
+                #print()
+
                 cofactor = coefficent_sign * coefficent * minor.calc_determinant()
                 det += cofactor
             return det
 
-        
-    
--- tests/matrix_test.py
+++ tests/matrix_test.py
@@ -6,30 +6,37 @@ A = Matrix([[1, 2],[3, 4]])
 B = Matrix([[1, 0, -2],[3, 4, 5],[0, 0, 1]])
 C = Matrix([[1, 2, 3], [4, 5, 6]])
 D = Matrix([[1, 2], [3, 4], [5, 6]])
-
+E = Matrix([[1, 2, 3, 4], [0, 1, 0, 2], [1, 1, 0, 4], [3, 0, 2, 1]])
+F = Matrix([[1, 2, 3, 4, 5], [1, 1, 2, 0, 0], [-1, -4, 0, 2, 0], [2, 3, 5, 6, 0], [1, 1, 1, 3, 0]])
 
 
 # transpose
-assert A.transpose() == [[1, 3],[2, 4]]
-assert B.transpose() == [[1, 3, 0], [0, 4, 0], [-2, 5, 1]]
-assert C.transpose() == [[1, 4], [2, 5], [3, 6]]
-assert D.transpose() == [[1, 3, 5], [2, 4, 6]]
+assert A.transpose().elements == [[1, 3],[2, 4]]
+assert B.transpose().elements == [[1, 3, 0], [0, 4, 0], [-2, 5, 1]]
+assert C.transpose().elements == [[1, 4], [2, 5], [3, 6]]
+assert D.transpose().elements == [[1, 3, 5], [2, 4, 6]]
 
 # add 
-assert A.add([[1, 1], [1, 1]]) == [[2, 4],[3, 5]]
-assert B.add([[1, 1, 1],[1, 1, 1], [1, 1, 1]]) == [[2, 4, 1], [1, 5, 1], [-1, 6, 2]]
+assert A.add([[1, 1], [1, 1]]).elements == [[2, 3],[4, 5]]
+assert B.add([[1, 1, 1],[0, 1, 3], [1, 1, -2]]).elements == [[2, 1, -1], [3, 5, 8], [1, 1, -1]]
 
 # subtract
-assert C.subtract([[-1, -2], [-3, 1], [1, 1]]) == [[2, 6], [5, 4], [2, 5]]
-assert D.subtract([[-2, -2, -2], [0, 0, 1]]) == [[3, 5, 7], [2, 4, 5]]
+assert C.subtract([[-1, -2,-3], [1, 1, 1]]).elements == [[2, 4, 6], [3, 4, 5]]
+assert D.subtract([[-2, -2], [-2, 0], [0, 1]]).elements == [[3, 4], [5, 4], [5, 5]]
 
 
 # scalar_multiply
-assert A.scalar_multiply(-2) == [[-4, -8],[-6, -10]]
-assert B.scalar_multiply(0) == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+assert A.scalar_multiply(-2).elements == [[-2, -4],[-6, -8]]
+assert B.scalar_multiply(0).elements == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 # matrix_multiply
-assert C.matrix_multiply([[2, 2], [2, 2]]) == [[16, 16], [18, 18], [14, 14]] 
-assert D.matrix_multiply([[1, 0, 1], [1, 1, 3], [2, -1, 0]]) == [[22, -2, 18], [16, -1, 14]]
+assert C.matrix_multiply([[2, 2], [2, 2], [1, 0]]).elements == [[9, 6], [24, 18]] 
+assert D.matrix_multiply([[1, -1], [0, 0]]).elements == [[1, -1], [3, -3], [5, -5]]
 
-
+#determinant
+assert A.calc_determinant() == -2
+assert B.calc_determinant() ==  4
+assert C.calc_determinant() ==  "not a valid input, matrix must be square"
+assert D.calc_determinant() ==  "not a valid input, matrix must be square"
+assert E.calc_determinant() == 11
+assert F.calc_determinant() == -175
