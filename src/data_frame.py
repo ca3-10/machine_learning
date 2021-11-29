d0a7c3bd045d42bfebc56032ed5bbc513ea6c179
ca3-10 <riseredasthedawn926@gmail.com>
2021-11-29 18:40:23 +0000

    things

--- src/data_frame.py
+++ src/data_frame.py
@@ -9,7 +9,7 @@ class DataFrame:
     def to_array(self):
         array = [[] for i in range(len(self.data_dict[self.column_order[0]]))]
         for values in self.column_order: 
-            for i in range(len(array)): 
+            for i in range(len(array)):
                 array[i].append(self.data_dict[values][i])
         return array 
     
@@ -20,17 +20,43 @@ class DataFrame:
         return DataFrame(copy_dict, column_order = columns)
     
     def select_rows(self, rows): 
+        selected_rows = []
+        array = self.to_array()
+        for row in rows: 
+            selected_rows.append(array[row])
         
+        return DataFrame(selected_rows, self.column_order)
     
+    @classmethod
+    def from_array(cls, arr, column_order):
+        dict = {}
+        for values in column_order:
+            dict[values] = []
+            for j in range(len(arr)):
+                dict[values].append(arr[j][column_order.index(values)])
+        return cls(dict, column_order = column_order)
+    
+    def to_json(self): 
+        arr_of_dicts = [{} for i in range(len(self.data_dict)+1)]
+        for i in range(len(arr_of_dicts)): 
+            for values in self.column_order:
+                arr_of_dicts[i][values] = self.data_dict[values][i]
+        return DataFrame(arr_of_dicts, column_order=self.column_order)
+    
+    #@classmethod
+    #def from_json(json, column_order):
 
-        
-
-        
 
     
 data_dict = {"Pete": [1, 0, 1, 0],"John": [2, 1, 0, 2],"Sarah": [3, 1, 4, 0]}
-df1 = DataFrame(data_dict,["John","Pete", "Sarah"])
+df1 = DataFrame(data_dict,["John","Sarah", "Pete"])
 #print(df1.to_array())
 df2 = df1.select_columns(["Sarah", "Pete"])
-print(df2.to_array())
-print(df1.to_array())
+df3 = df1.select_rows([1,3])
+
+columns = ['firstname', 'lastname', 'age']
+arr = [['Kevin', 'Fray', 5],['Charles', 'Trapp', 17],[
+    'Anna', 'Smith', 13],['Sylvia', 'Mendez', 9]]
+df = DataFrame.from_array(arr, columns)
+print(df.to_json())
+
