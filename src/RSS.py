9159f41f70cde5db40453c4490655d14451d11c2
ca3-10 <riseredasthedawn926@gmail.com>
2022-01-11 16:58:14 +0000

    test cases(not done)

--- src/RSS.py
+++ src/RSS.py
@@ -24,15 +24,3 @@ def RSS_parabola(a,b,c,a_gradient,b_gradient, c_gradient, learning_rate = 0.01, 
         c_old = b_new
     return (a_new, b_new, c_new)    
 
-def a_grad(a,b,c): 
-    return ((196 * a) + (72 * b) + (38 * c) -26)
-
-def b_grad(a,b,c): 
-    return ((72 * a) + (28 * b) + (12 * c) -10)
-
-def c_grad(a,b,c): 
-    return ((28 * a) + (12 * b) + (8 * c) - 8)
-
-
-parobola = RSS_parabola(1,0,0,a_grad, b_grad, c_grad, learning_rate=0.00001, num_interations=1000000)
-print(parobola)
--- tests/RSS_tests.py
+++ tests/RSS_tests.py
@@ -3,28 +3,62 @@ import sys
 sys.path.insert(1, sys.path[0].replace('tests', 'src'))
 from RSS import *
 
+# LINEAR 
 
-def b_one_grad(a, b):
+def b_one_grad_lin(a, b):
     return ((10 * a) + (6 * b) - 18)
 
-def a_one_grad(a,b): 
+def a_one_grad_lin(a,b): 
     return ((26 * a) + (10 * b) - 42)
 
-rss_one = RSS(1, 0, a_one_grad, b_one_grad, learning_rate=0.01, num_interations = 1000000)
+line_one = RSS(1, 0, a_one_grad_lin, b_one_grad_lin, learning_rate=0.01, num_interations = 1000000)
 
-assert round(rss_one[0], 4) == 1.2857
-assert round(rss_one[1], 4) == 0.8571
+assert round(line_one[0], 4) == 1.2857
+assert round(line_one[1], 4) == 0.8571
 
-#-------------------
+#LINEAR 
+
+def a_two_grad_lin(a, b):
+    return (28 * a) + (12 * b) + 2
+
+def b_two_grad_lin(a,b): 
+    return (12 * a) + (8 * b) - 4
+
+line_two = RSS(1, 0, a_two_grad_lin, b_two_grad_lin, learning_rate=0.01, num_interations = 1000000)
+
+assert round(line_two[0], 2) == -0.8
+assert round(line_two[1], 2) == 1.7
+
+#PARABOLA 
+
+def a_one_grad_pb(a,b,c): 
+    return ((196 * a) + (72 * b) + (38 * c) -26)
+
+def b_one_grad_pb(a,b,c): 
+    return ((72 * a) + (28 * b) + (12 * c) -10)
+
+def c_one_grad_pb(a,b,c): 
+    return ((28 * a) + (12 * b) + (8 * c) - 8)
+
+parobola = RSS_parabola(1,0,0,a_one_grad_pb, b_one_grad_pb, c_one_grad_pb, learning_rate=0.00001, num_interations=1000000)
 
 
-def a_two_grad(a, b):
-    return 
+#PARABOLA
+def a_two_grad_pb(a,b,c): 
+    return ((64 * a) + (32 * b) + (16 * c) -16)
 
-def b_two_grad(a,b): 
-    return 
+def b_two_grad_pb(a,b,c): 
+    return ((32 * a) + (16 * b) + (8 * c) -8)
 
-rss_two = RSS(1, 0, a_two_grad, b_two_grad, learning_rate=0.01, num_interations = 1000000)
+def c_two_grad_pb(a,b,c): 
+    return ((16 * a) + (8 * b) + (8 * c) - 8)
+
+parobola_2 = RSS_parabola(1,0,0,a_two_grad_pb, b_two_grad_pb, c_two_grad_pb, learning_rate=0.001, num_interations=1000000)
+print(parobola_2)
+
+
+
+
 
 
 
